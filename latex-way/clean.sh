2fbd39bcc5ad474660beecacde28c01fb0c193cf
riparuk <faruqirifa@gmail.com>
2024-01-29 17:49:07 +0700

    Easier the flow

---
+++ latex-way/clean.sh
@@ -0,0 +1,4 @@
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+rm $SCRIPT_DIR/WhatsApp*.zip
+rm $SCRIPT_DIR/images/* $SCRIPT_DIR/*.tex $SCRIPT_DIR/*.log $SCRIPT_DIR/*.aux 
--- latex-way/images/README.md
+++
@@ -1,1 +0,0 @@
-Here is the directory default of images stored
